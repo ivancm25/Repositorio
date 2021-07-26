bef116b17f8e87586831974b276676cee696f37c
Ivan <ivancuinas@gmail.com>
2021-07-26 17:56:57 +0200

    progreso servicio usuarios

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,6 +10,7 @@ import { UsuariosService } from 'src/app/services/usuarios/usuarios.service';
 export class HeaderComponent implements OnInit {
 
   usuarioForm: FormGroup;
+  usuarios: any;
 
   constructor(
     public fb: FormBuilder,
@@ -19,15 +20,49 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
 
     this.usuarioForm = this.fb.group({
+      id : [''],
       nombre : ['', Validators.required],
 		  apellido : ['', Validators.required],
 		  email : ['', Validators.required],
 		  contrasena : ['', Validators.required]
     })
+
+    this.usuarioService.getAllUsuarios().subscribe(resp=>{
+      this.usuarios = resp;
+    },
+      error=>{ console.error(error) }
+    )
   }
 
   public guardarUsuario(): void {
+    this.usuarioService.saveUsuario(this.usuarioForm.value).subscribe(resp=>{
+      this.usuarioForm.reset();
+      this.usuarios = this.usuarios.filter((usuario: { id: any; })=> resp.id!=usuario.id);
+      this.usuarios.push(resp);
+    },
+      error=>{ console.error(error) }
+    )
+  }
 
+  public eliminar(usuario:any) {
+    this.usuarioService.deleteUsuario(usuario.id).subscribe(resp=>{
+      if (resp === true) {
+        this.usuarios.pop(usuario);
+      }
+      this.usuarioForm.reset();
+    },
+      error=>{ console.error(error) }
+    )
+  }
+
+  public editar(usuario:any) {
+    this.usuarioForm.setValue({
+      id : usuario.id,
+      nombre : usuario.nombre,
+		  apellido : usuario.apellido,
+		  email : usuario.email,
+		  contrasena : usuario.contrasena
+    })
   }
 
 }
--- src/app/services/usuarios/usuarios.service.ts
+++ src/app/services/usuarios/usuarios.service.ts
@@ -14,4 +14,12 @@ export class UsuariosService {
   public getAllUsuarios(): Observable<any>{
     return this.httpClient.get(this.API_SERVER);
   }
+
+  public saveUsuario (usuario:any): Observable<any>{
+    return this.httpClient.post(this.API_SERVER, usuario);
+  }
+
+  public deleteUsuario(id:any): Observable<any>{
+    return this.httpClient.delete(this.API_SERVER + "delete/" + id);
+  }
 }
